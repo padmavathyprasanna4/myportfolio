2d62b31beef21fe23130fc7d3fa4cc0256d53b69
unknown <you@example.com>
2026-02-04 12:46:22 +0800

    Fixed file path and restored resume file

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -27,7 +27,7 @@ const Home = () => {
 
           {/* Downloads CV */}
           <a
-            href="src/assets/Padmavathy_Gangadaran_Latest Resume.pdf"
+            href="public/assets/Padmavathy_Gangadaran_LatestResume.pdf"
             download
             className="btn btn-outline-light ms-3"
           >
